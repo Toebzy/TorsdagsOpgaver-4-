6cdd9c98cbeb2ae0b0be48cd99e21b8ce22b3595
Toebzy <Tobiascarlsen@live.dk>
2022-09-29 11:36:39 +0200

    Task 1

---
+++ TorsdagsOpgaver 4/Task 1/src/Course.java
@@ -0,0 +1,21 @@
+public class Course {
+
+
+    private String name;
+
+    public Course(String name){
+        public String getName() {
+            return name;
+        }
+
+
+
+        public String toString() {
+            return "Course: "+
+                    "name=" + getName();
+
+
+    }
+
+
+}
---
+++ TorsdagsOpgaver 4/Task 1/src/TestStudents.java
@@ -0,0 +1,28 @@
+public class TestStudents {
+
+    public static void main(String[] args){
+
+
+        Student 1 = new Student("Tobias");
+        Student 2 = new Student("Simone");
+
+        Course math = new Course("math 101");
+        Course greece = new Course("greek");
+        Course programmering = new Course("programming");
+        
+
+        //students
+        System.out.println(1);
+        System.out.println(2);
+
+        //courses
+        System.out.println(math);
+        System.out.println(greece);
+        System.out.println(programmering);
+
+
+
+
+
+    }
+}
